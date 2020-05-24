22b93e422c383690a1e295d48176aa6527545d72
Andrey <andrey.kappix.haines@gmail.com>
2020-05-24 17:44:33 +0300

    добавление таблицы и страницы добавления студентов

---
+++ LabberClient/Students/StudentsTable/StudentsTablePage.xaml.cs
@@ -0,0 +1,16 @@
+﻿using LabberClient.VMStuff;
+using LabberLib.DataBaseContext.Entities;
+using System.Collections.ObjectModel;
+using System.Windows.Controls;
+
+namespace LabberClient.Students.StudentsTable
+{
+    public partial class StudentsTablePage : Page
+    {
+        public StudentsTablePage(ObservableCollection<Student> students, ResponseHandler responseEvent, PageEnabledHandler pageEnabledEvent, LoadingStateHandler loadingStateEvent, CompleteStateHanlder completeStateEvent)
+        {
+            InitializeComponent();
+            DataContext = new StudentsTablePageVM(students, responseEvent, pageEnabledEvent, loadingStateEvent, completeStateEvent);
+        }
+    }
+}
