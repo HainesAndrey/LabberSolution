34c954398d782fa489aab92bb5d020000aaa0a53
Andrey <andrey.kappix.haines@gmail.com>
2020-05-24 17:45:33 +0300

    vm для таблицы студентов

---
+++ LabberClient/Students/StudentsTable/StudentsTablePageVM.cs
@@ -0,0 +1,15 @@
+﻿using LabberClient.VMStuff;
+using LabberLib.DataBaseContext.Entities;
+using System.Collections.ObjectModel;
+
+namespace LabberClient.Students.StudentsTable
+{
+    public class StudentsTablePageVM : LabberVMBase
+    {
+        public StudentsTablePageVM(ObservableCollection<Student> students, ResponseHandler ResponseEvent, PageEnabledHandler PageEnabledEvent, LoadingStateHandler LoadingStateEvent, CompleteStateHanlder CompleteStateEvent)
+            : base(ResponseEvent, PageEnabledEvent, LoadingStateEvent, CompleteStateEvent)
+        {
+
+        }
+    }
+}
