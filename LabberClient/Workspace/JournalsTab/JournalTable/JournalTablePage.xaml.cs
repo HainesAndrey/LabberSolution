2fcb5d622fbb14d90bfa4f0813ce5eb2ba274450
Andrey <andrey.kappix.haines@gmail.com>
2020-05-25 21:03:56 +0300

    создание файлов таблицы журнала

---
+++ LabberClient/Workspace/JournalsTab/JournalTable/JournalTablePage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using LabberClient.VMStuff;
+using LabberLib.DataBaseContext.Entities;
+using System.Windows.Controls;
+
+namespace LabberClient.Workspace.JournalsTab.JournalTable
+{
+    public partial class JournalTablePage : Page
+    {
+        public JournalTablePage(Journal journal, ResponseHandler ResponseEvent, PageEnabledHandler PageEnabledEvent, LoadingStateHandler LoadingStateEvent, CompleteStateHanlder CompleteStateEvent)
+        {
+            InitializeComponent();
+            DataContext = new JournalTablePageVM(journal, ResponseEvent, PageEnabledEvent, LoadingStateEvent, CompleteStateEvent);
+        }
+    }
+}
---
+++ LabberClient/Workspace/JournalsTab/JournalTable/JournalTablePageVM.cs
@@ -0,0 +1,16 @@
+﻿using LabberClient.VMStuff;
+using LabberLib.DataBaseContext.Entities;
+
+namespace LabberClient.Workspace.JournalsTab.JournalTable
+{
+    public class JournalTablePageVM : LabberVMBase
+    {
+        public Journal Journal { get; set; }
+
+        public JournalTablePageVM(Journal journal, ResponseHandler ResponseEvent, PageEnabledHandler PageEnabledEvent, LoadingStateHandler LoadingStateEvent, CompleteStateHanlder CompleteStateEvent)
+            : base(ResponseEvent, PageEnabledEvent, LoadingStateEvent, CompleteStateEvent)
+        {
+            Journal = journal;
+        }
+    }
+}
