5df8cd24c6cd2fbf2e65f372b75eed8b251e2f14
Andrey <andrey.kappix.haines@gmail.com>
2020-05-25 14:12:43 +0300

    begining of WorkspacePage

--- LabberClient/Workspace/WorkspacePageVM.cs
+++ LabberClient/Workspace/WorkspacePageVM.cs
@@ -1,13 +1,19 @@
 ﻿using LabberClient.VMStuff;
+using LabberClient.Workspace.JournalsTab;
 
 namespace LabberClient.Workspace
 {
     public class WorkspacePageVM : LabberVMBase
     {
+        public JournalsTabPage JournalsTabPage { get; set; }
+        public int LabsTabPage { get; set; }
+        public int DebtsTabPage { get; set; }
+        public int AdminTabPage { get; set; }
+
         public WorkspacePageVM(ResponseHandler ResponseEvent, PageEnabledHandler PageEnabledEvent, LoadingStateHandler LoadingStateEvent, CompleteStateHanlder CompleteStateEvent)
             : base(ResponseEvent, PageEnabledEvent, LoadingStateEvent, CompleteStateEvent)
         {
-
+            JournalsTabPage = new JournalsTabPage(InvokeResponseEvent, InvokePageEnabledEvent, InvokeLoadingStateEvent, InvokeCompleteStateEvent);
         }
     }
 }
