efe463113ef1c0ec6219a00f6eddf3dbdd668358
Andrey <andrey.kappix.haines@gmail.com>
2020-05-25 14:08:27 +0300

    создание JournalsTabPage

---
+++ LabberClient/Workspace/JournalsTab/JournalsTabPage.xaml.cs
@@ -0,0 +1,14 @@
+﻿using LabberClient.VMStuff;
+using System.Windows.Controls;
+
+namespace LabberClient.Workspace.JournalsTab
+{
+    public partial class JournalsTabPage : Page
+    {
+        public JournalsTabPage(ResponseHandler ResponseEvent, PageEnabledHandler PageEnabledEvent, LoadingStateHandler LoadingStateEvent, CompleteStateHanlder CompleteStateEvent)
+        {
+            InitializeComponent();
+            DataContext = new JournalsTabPageVM(ResponseEvent, PageEnabledEvent, LoadingStateEvent, CompleteStateEvent);
+        }
+    }
+}
---
+++ LabberClient/Workspace/JournalsTab/JournalsTabPageVM.cs
@@ -0,0 +1,13 @@
+﻿using LabberClient.VMStuff;
+
+namespace LabberClient.Workspace.JournalsTab
+{
+    public class JournalsTabPageVM : LabberVMBase
+    {
+        public JournalsTabPageVM(ResponseHandler ResponseEvent, PageEnabledHandler PageEnabledEvent, LoadingStateHandler LoadingStateEvent, CompleteStateHanlder CompleteStateEvent)
+            : base(ResponseEvent, PageEnabledEvent, LoadingStateEvent, CompleteStateEvent)
+        {
+
+        }
+    }
+}
