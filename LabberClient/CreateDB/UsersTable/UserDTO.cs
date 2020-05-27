3d5bb69359a5737980c7da46f00a5c719bce0b62
Andrey <andrey.kappix.haines@gmail.com>
2020-05-27 21:45:45 +0300

    change directory

---
+++ LabberClient/CreateDB/UsersTable/UserDTO.cs
@@ -0,0 +1,18 @@
+﻿using LabberLib.DataBaseContext.Entities;
+
+namespace LabberClient.CreateDB.UsersTable
+{
+    public class UserDTO
+    {
+        public User User { get; set; } = new User();
+        public bool IsAdmin { get; set; }
+
+        public UserDTO() { }
+
+        public UserDTO(User user)
+        {
+            User = user;
+            IsAdmin = user.RoleId == 1;
+        }
+    }
+}
